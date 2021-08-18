0939a2c66c50321bae55349ff61253705d57a32d
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-08-18 17:48:23 +0300

    orders get

--- controllers/order.js
+++ controllers/order.js
@@ -1,4 +1,50 @@
+const { models } = require("mongoose");
 const Order = require("../models/order_model");
 
+const date = new Date();
+const randomnum = Math.floor(Math.random() * 9999) + 1000;
 
-module.exports={}; //EXPORT YOUR FUNCTIONS HERE
+const orderget = function(req, res){
+    Order
+        .find()
+        .then((orders)=>{
+            res.status(201).json(orders);
+        })
+        .catch((err) => {
+            res.json(
+                {message: err.message}
+            );
+        });
+
+};
+const orderPost = function(req, res){
+   new Order({
+        status : req.body.status,
+        progress : req.body.progress,
+        is_completed : req.body.is_completed,
+        order_created_date : req.body.order_created_date,
+        order_completed_date : req.body.order_completed_date,
+        start_time : req.body.start_time,
+        saved_time: req.body.saved_time,
+        unique_code : req.body.unique_code,
+        seeker_id : req.body.seeker_id,
+        provider_id : req.body.provider_id,
+        final_payment: req.body.final_payment,
+
+    })
+    
+    .save()
+    .then((orders)=>{
+        res.status(201).json({
+            order : orders,
+            message: `order Successfully`
+        });
+    })
+   
+    .catch((err)=>{
+        res.json(
+            {message: err.message}
+        );
+    })
+}
+module.exports={orderPost, orderget}; //EXPORT YOUR FUNCTIONS HERE
--- routes/provider.js
+++ routes/provider.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const Provider = require("../controllers/provider");
-const { getProviderById } = require("../controllers/provider");
+// const express = require("express");
+// const Provider = require("../controllers/provider");
+// const { getProviderById } = require("../controllers/provider");
 
-var router = express.Router();
+// var router = express.Router();
 
-router.get("/:id", getProviderById);
-router.get("/providers/category/:category_id", Provider.providerCategory);
-router.get("/providers/top", Provider.topProviders);
-router.get("/providers/top/:category_id", Provider.topCategoryProviders);
+// router.get("/:id", getProviderById);
+// router.get("/providers/category/:category_id", Provider.providerCategory);
+// router.get("/providers/top", Provider.topProviders);
+// router.get("/providers/top/:category_id", Provider.topCategoryProviders);
 
-module.exports = router;
+// module.exports = router;
