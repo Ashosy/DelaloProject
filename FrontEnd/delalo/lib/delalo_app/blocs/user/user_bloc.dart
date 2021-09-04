3d94d3831d0fd0053f1ff096a6ce3c5ddaff6858
Ayda <se.ayda.sultan@gmail.com>
2021-09-04 17:18:12 +0300

    user_bloc done

--- FrontEnd/delalo/lib/delalo_app/blocs/user/user_bloc.dart
+++ FrontEnd/delalo/lib/delalo_app/blocs/user/user_bloc.dart
@@ -10,7 +10,28 @@ class UserBloc extends Bloc<UserEvent, UserState>{
   UserBloc({@required this.userRepository})
       : assert(userRepository != null),
         super(UserLoading());
-  @override
+   @override
+  Stream<UserState> mapEventToState(UserEvent event) async* {
+    
 
+    if (event is UserUpdate) {
+      try {
+        await userRepository.updateCourse(event.user);
+        final courses = await userRepository.getCourses();
+        yield UserLoadSuccess(courses);
+      } catch (_) {
+        yield CourseOperationFailure();
+      }
+    }
 
+    if (event is UserDelete) {
+      try {
+        await userRepository.deleteCourse(event.user.id);
+        final user = await userRepository.getUser();
+        yield UserLoadSuccess(user);
+      } catch (_) {
+        yield CourseOperationFailure();
+      }
+    }
+  }
 }
