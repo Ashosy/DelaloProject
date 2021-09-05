9a97c0ccb8ef88b11fb3f850527b3f767862741d
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-09-05 23:30:35 +0300

    [Frontend] admin data provider updated

---
+++ FrontEnd/delalo/lib/delalo_app/data_provider/admin_data/provider_data.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:delalo/delalo_app/models/user.dart';
+import 'package:http/http.dart' as http;
+class AdminProviderDataProvider {
+  final _baseurl = 'http://127.0.0.1:3000';
+  final http.Client httpClient;
+  AdminProviderDataProvider({required this.httpClient})
+      : assert(httpClient != null);
+
+  Future<List> getProvidersFromProvider() async{
+    try{
+      final response = await httpClient.get(Uri.https(_baseurl, "/provider"));
+      if(
+        response.statusCode == 200
+      ){
+        final providers = jsonDecode(response.body) as List;
+        return providers.map((provider) => User.fromJson(provider)).toList();
+        
+      }
+      else{
+        throw Exception("Error");
+      }
+    }on SocketException catch (e) {
+      throw e;
+    } on HttpException catch (e) {
+      throw e;
+    }
+
+  }
+   Future<List> getProviderFromProvider(String id) async{
+    try{
+      final response = await httpClient.get(Uri.https(_baseurl, "/provider/:$id"));
+      if(
+        response.statusCode == 200
+      ){
+        final providers = jsonDecode(response.body) as List;
+        return providers.map((provider) => User.fromJson(provider)).toList();
+        
+      }
+      else{
+        throw Exception("Error");
+      }
+    }on SocketException catch (e) {
+      throw e;
+    } on HttpException catch (e) {
+      throw e;
+    }
+
+  }
+
+
+}
