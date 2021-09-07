b961a2ad9afbca5dbda361d40e6f73f9d56f70a0
Biniyam <se.biniyam.abiy@gmail.com>
2021-09-07 21:56:57 +0300

    added login form screen

--- FrontEnd/delalo/lib/delalo_app/screens/allScreens.dart
+++ FrontEnd/delalo/lib/delalo_app/screens/allScreens.dart
@@ -1,1 +1,2 @@
 export 'singleProviderPage.dart';
+export 'loginScreen.dart';
--- FrontEnd/delalo/lib/routeGenerator.dart
+++ FrontEnd/delalo/lib/routeGenerator.dart
@@ -1,16 +1,19 @@
+import 'package:delalo/delalo_app/screens/loginScreen.dart';
 import 'package:flutter/material.dart';
 import 'delalo_app/screens/allScreens.dart';
 
-
 class RouteGenerator {
- static const String singleProviderPageName = "/";
+  static const String singleProviderPageName = "/";
+  static const String loginScreenName = "/loginScreen";
 
- static Route<dynamic> generateRoute(RouteSettings settings) {
-   switch (settings.name) {
-     case singleProviderPageName:
-       return MaterialPageRoute(builder: (_) => ProviderProfile());
-     default:
-       throw FormatException("Route was not found");
-   }
- }
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case singleProviderPageName:
+        return MaterialPageRoute(builder: (_) => ProviderProfile());
+      case loginScreenName:
+        return MaterialPageRoute(builder: (_) => LoginForm());
+      default:
+        throw FormatException("Route was not found");
+    }
+  }
 }
