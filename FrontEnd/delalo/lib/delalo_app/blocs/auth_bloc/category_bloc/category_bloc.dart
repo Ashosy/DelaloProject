eb7673ff094303ce647e56c4046b37d268324134
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-09-08 14:01:58 +0300

    [Frontend] UI Admin category added

---
+++ FrontEnd/delalo/lib/delalo_app/blocs/auth_bloc/category_bloc/category_bloc.dart
@@ -0,0 +1,36 @@
+import 'package:delalo/delalo_app/repository/admin_repository/category_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'category_event.dart';
+import 'category_state.dart';
+
+class CategoryBloc extends Bloc<CategoryEvent, CategoryState> {
+  final AdminCategoryRepository categoryRepository;
+
+  CategoryBloc({required this.categoryRepository}) : super(CategoryLoading());
+
+  @override
+  Stream<CategoryState> mapEventToState(CategoryEvent event) async* {
+    // TO DO: implement mapEventToState
+    if (event is LoadAllCategories) {
+      yield CategoryLoading();
+      try {
+        final categories = await categoryRepository.getCategoriesFromCategory();
+        yield CategoryLoaded(categories);
+      } catch (_) {
+        yield CategoryOperationFailed();
+      }
+    }
+    if (event is AddCategory) {
+      try {
+        final category = event.category;
+        await categoryRepository.createCategory(category);
+        final newcategory =
+            await categoryRepository.getCategoriesFromCategory();
+        yield CategoryLoaded(newcategory);
+      } catch (_) {
+        yield CategoryOperationFailed();
+      }
+    }
+  }
+}
---
+++ FrontEnd/delalo/lib/delalo_app/blocs/auth_bloc/category_bloc/category_event.dart
@@ -0,0 +1,27 @@
+
+import 'package:delalo/delalo_app/models/models.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class CategoryEvent extends Equatable {
+  const CategoryEvent();
+}
+
+class LoadAllCategories extends CategoryEvent {
+  const LoadAllCategories();
+
+  @override
+  // TO DO: implement props
+  List<Object> get props => [];
+}
+
+class AddCategory extends CategoryEvent {
+  final Category category;
+  const AddCategory(this.category);
+
+  @override
+  // TO DO: implement props
+  List<Object> get props => [category];
+
+  @override
+  String toString() => 'Added Category {category: $category}';
+}
