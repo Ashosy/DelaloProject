8dc451e1ab076bfee3ad936ecee13f4a3a20ea18
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-09-09 11:51:09 +0300

    [Frontend] category and providers added orders  list for Admin

---
+++ FrontEnd/delalo/lib/delalo_app/screens/admin/adminScreens.dart
@@ -0,0 +1,44 @@
+
+
+import 'package:delalo/delalo_app/screens/admin/providers_list.dart';
+import 'package:flutter/material.dart';
+
+import 'category_list.dart';
+import 'orders_list.dart';
+
+class AdminScreen extends StatefulWidget {
+   static const routeName = '/';
+
+  @override
+  _AdminScreenState createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(title: Text("Delalo",
+        textAlign: TextAlign.center,
+        ),centerTitle: true,
+        backgroundColor: Colors.purple,
+        bottom: TabBar(tabs: [
+         Tab(text: 'Category'),
+         Tab(text: 'Providers'),
+         Tab(text: 'Order')
+        ],),),
+       
+        body: TabBarView(
+          children: [
+          AdminCategoryList(),
+          AdminProvidersList(),
+          AdminOrdersJobs()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+ 
---
+++ FrontEnd/delalo/lib/delalo_app/screens/admin/providers_list.dart
@@ -0,0 +1,58 @@
+import 'package:delalo/delalo_app/blocs/admin_bloc/provider_bloc/provider_bloc.dart';
+import 'package:delalo/delalo_app/blocs/admin_bloc/provider_bloc/provider_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AdminProvidersList extends StatefulWidget {
+  const AdminProvidersList({Key? key}) : super(key: key);
+
+  @override
+  _AdminProvidersListState createState() => _AdminProvidersListState();
+}
+
+class _AdminProvidersListState extends State<AdminProvidersList> {
+  @override
+  Widget build(BuildContext context) {
+    final providersBloc = BlocProvider.of<ProvidersBloc>(context);
+    return Scaffold(
+        
+        body: 
+        Center(
+          child: BlocBuilder<ProvidersBloc, ProvidersState>(
+              builder: (_, providersState) {
+            if (providersState is ProviderLoading) {
+              return CircularProgressIndicator();
+            } 
+            else if (providersState is ProvidersOperationsFailed) {
+              return Text("Loading failed");
+            } 
+            else if (providersState is ProviderLoaded) {
+              final providers = providersState.providers;
+              return ListView.builder(
+                  itemCount: providers.length,
+                  itemBuilder: (_, index) {
+                    final provider = providers[index];
+                    return Container(
+                      //  color: Colors.grey[100],
+                       
+                      child: ListTile(
+                        
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage('https://picsum.photos/250?image=9')
+                          
+                            ,),
+                        title: Text(provider.firstname),
+                        subtitle: Text(provider.lastname),
+                      ),);
+                    
+                  });
+            }
+            return Container();
+          }),
+        ),
+        
+        
+        );
+  
+  }
+}
