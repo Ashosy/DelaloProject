0b7188bb834c102dd176517b7ec57341f8057a61
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-09-09 11:27:45 +0300

    [Backend] user model update

--- Backend/app.js
+++ Backend/app.js
@@ -7,13 +7,7 @@ const dotenv = require('dotenv');
 const DB_URI = process.env.MONGO_URI || "mongodb://localhost:27017/delalodb";
 const app = express();
 
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-app.use(cors(corsOptions));
+
 
 dotenv.config();
 const cors=require("cors");
@@ -23,7 +17,6 @@ const corsOptions ={
    optionSuccessStatus:200,
 }
 app.use(cors(corsOptions));
-
 app.use(express.json());
 app.use("/", router);
 mongoose.connect(DB_URI, {
