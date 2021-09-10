deab491cbeb04e862ee1220396c44ce14d9a45e2
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-09-10 17:35:52 +0300

    [frontend] providers_list Admin Ui done

--- FrontEnd/delalo/lib/delalo_app/screens/admin/providers_list.dart
+++ FrontEnd/delalo/lib/delalo_app/screens/admin/providers_list.dart
@@ -3,6 +3,7 @@ import 'package:delalo/delalo_app/blocs/admin_bloc/provider_bloc/provider_state.
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+
 class AdminProvidersList extends StatefulWidget {
   const AdminProvidersList({Key? key}) : super(key: key);
 
@@ -15,44 +16,40 @@ class _AdminProvidersListState extends State<AdminProvidersList> {
   Widget build(BuildContext context) {
     final providersBloc = BlocProvider.of<ProvidersBloc>(context);
     return Scaffold(
-        
-        body: 
-        Center(
-          child: BlocBuilder<ProvidersBloc, ProvidersState>(
-              builder: (_, providersState) {
-            if (providersState is ProviderLoading) {
-              return CircularProgressIndicator();
-            } 
-            else if (providersState is ProvidersOperationsFailed) {
-              return Text("Loading failed");
-            } 
-            else if (providersState is ProviderLoaded) {
-              final providers = providersState.providers;
-              return ListView.builder(
-                  itemCount: providers.length,
-                  itemBuilder: (_, index) {
-                    final provider = providers[index];
+      body: Center(
+        child: BlocBuilder<ProvidersBloc, ProvidersState>(
+            builder: (_, providersState) {
+          if (providersState is ProviderLoading) {
+            return CircularProgressIndicator();
+          } else if (providersState is ProvidersOperationsFailed) {
+            return Text("Loading failed");
+          } else if (providersState is ProviderLoaded) {
+            final providers = providersState.providers;
+            return ListView.builder(
+                itemCount: providers.length,
+                itemBuilder: (_, index) {
+                  final provider = providers[index];
+                  if (provider.role == 'provider') {
                     return Container(
                       //  color: Colors.grey[100],
-                       
+
                       child: ListTile(
-                        
                         leading: CircleAvatar(
-                          backgroundImage: NetworkImage('https://picsum.photos/250?image=9')
-                          
-                            ,),
+                          backgroundImage:
+                              NetworkImage('https://picsum.photos/250?image=9'),
+                        ),
                         title: Text(provider.firstname),
                         subtitle: Text(provider.lastname),
-                      ),);
-                    
-                  });
-            }
-            return Container();
-          }),
-        ),
-        
-        
-        );
-  
+                      ),
+                    );
+                  } else {
+                    return Container();
+                  }
+                });
+          }
+          return Container();
+        }),
+      ),
+    );
   }
 }
