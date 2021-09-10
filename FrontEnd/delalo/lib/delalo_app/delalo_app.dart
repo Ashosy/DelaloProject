ddf5f7a7f36e125e560a9f9d26882cfec0dc761e
Selamawit926 <se.selamawit.elias@gmail.com>
2021-09-10 13:49:15 -0700

    [Frontend] Search functionality working for categories

--- FrontEnd/delalo/lib/delalo_app/delalo_app.dart
+++ FrontEnd/delalo/lib/delalo_app/delalo_app.dart
@@ -7,6 +7,7 @@ import 'package:delalo/delalo_app/repository/auth_repository/login_repository.da
 import 'package:delalo/delalo_app/repository/auth_repository/signupProvider_repository.dart';
 import 'package:delalo/delalo_app/repository/auth_repository/signupUser_repository.dart';
 import 'package:delalo/delalo_app/repository/user_repository/order_jobs_repository.dart';
+import 'package:delalo/delalo_app/repository/user_repository/search_repository.dart';
 import 'package:delalo/delalo_app/screens/navigation_drawer/navigation.dart';
 import 'package:delalo/routeGenerator.dart';
 import 'package:flutter/material.dart';
@@ -15,8 +16,10 @@ import 'package:http/http.dart' as http;
 import 'package:delalo/delalo_app/repository/user_repository/category_repository.dart';
 import 'blocs/auth_bloc/signupProvider_bloc/signupProvider_bloc.dart';
 import 'blocs/auth_bloc/signupUser_bloc/signupUser_bloc.dart';
+import 'blocs/search_bloc/search_bloc.dart';
 import 'data_provider/auth_data/singupProvider_data.dart';
 import 'data_provider/user_data/category_data.dart';
+import 'data_provider/user_data/search_data.dart';
 
 class MyApp extends StatelessWidget {
   static final httpClient = http.Client();
@@ -36,6 +39,7 @@ class MyApp extends StatelessWidget {
     httpClient: http.Client(),
     ),
   );
+  final searchRepository= SearchCategoryRepository(searchCategoryDataProvider: SearchCategoryDataProvider(httpClient: http.Client()) );
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
@@ -60,6 +64,10 @@ class MyApp extends StatelessWidget {
                       ..add(
                         CategoriesLoad(),
                       ),
+
+        ),
+        BlocProvider<SearchBloc>(
+        create: (context)=> SearchBloc(searchRepository: searchRepository)
         )
       ],
       child: MaterialApp(
--- FrontEnd/delalo/lib/delalo_app/screens/category_page.dart
+++ FrontEnd/delalo/lib/delalo_app/screens/category_page.dart
@@ -1,3 +1,6 @@
+import 'package:delalo/delalo_app/blocs/search_bloc/search_bloc.dart';
+import 'package:delalo/delalo_app/blocs/search_bloc/search_event.dart';
+import 'package:delalo/delalo_app/blocs/search_bloc/search_state.dart';
 import 'package:flutter/material.dart';
 // import 'package:delalo/routeGenerator.dart';
 import 'package:flutter/cupertino.dart';
@@ -17,12 +20,27 @@ class CategoryPage extends StatefulWidget {
 }
 
 class _CategoryPageState extends State<CategoryPage> {
+ 
   @override
   Widget build(BuildContext context) {
     // final categoryBloc = BlocProvider.of<CategoryBloc>(context);
+     final searchBloc=BlocProvider.of<SearchBloc>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Cateogories"),
+        actions: <Widget>[
+          IconButton(
+                  onPressed: (){
+                    showSearch(
+                    context: context, 
+                    delegate: SearchCategory(
+                      searchBloc: searchBloc
+                      )
+                    );
+                  },
+                  icon: Icon(Icons.search),
+                )
+        ],
+        
       ),
       body: BlocBuilder<CategoryBloc, CategoryState>(
         builder: (_, CategoryState) {
@@ -68,4 +86,96 @@ class _CategoryPageState extends State<CategoryPage> {
       ),
     );
   }
+}
+
+
+class SearchCategory extends SearchDelegate<List>{
+  SearchBloc searchBloc;
+  SearchCategory({required this.searchBloc});
+ 
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: (){
+          query='';
+        }, 
+        icon: Icon(Icons.clear),
+      )
+    ];
+      
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: (){
+        close(context,[]);
+        },
+      icon: Icon(Icons.arrow_back_ios)
+     
+     );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+      searchBloc.add(Search(query:query));
+      print(query);
+      return BlocBuilder<SearchBloc,SearchState>(
+        builder: (BuildContext context,SearchState state) {
+          print(state);
+          if(state is SearchUninitialized){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          else if(state is SearchError){
+            return Center(
+              child: Text("Failed to load"),
+            );
+          }
+          else if(state is SearchLoaded){
+            if(state.categories.isEmpty){
+              return Center(
+                child: Text("No results found"),
+              );
+            }
+            else{
+               return ListView.builder(
+                itemCount: state.categories.length,
+                itemBuilder: (_, index) {
+                  final category = state.categories[index];
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: Card(
+                      color: Colors.purple[50],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      shadowColor: Colors.black,
+                      child: ListTile(
+                        title: Text(category.description),
+                        subtitle: Text(
+                            'Number of providers: ${category.numOfProviders}'),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                        ),
+                      ),
+                    ),
+                  );
+                }
+                );
+              }
+          }
+          return Container();
+        }
+      );
+  }
+              
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Container();
+  }
+  
+
 }
